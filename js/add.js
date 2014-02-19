3306a093c5dd8ad6ae65f7b3260384c7aa3e8014
Gabriel Carmo <gabriel.o.carmo@gmail.com>
2014-02-19 16:51:38 -0300

    primeira parte de transformar um adicionar receita e despesa numa pagina só

--- js/add.js
+++ js/add.js
@@ -1,8 +1,40 @@
 $(function() {
 
-  $("#show-form-tipo-despesa").click(function() {
+  var tempoAddTipo = 300;
+
+  $("#show-form-tipo-despesa").click(function(event) {
     event.preventDefault();
-    $(".form-tipo-despesa").toggle(100);
+    if($(".add-tipo-receita").css('display') != 'none') {
+      $("#show-form-tipo-receita").removeClass('is-active');
+      $(".add-tipo-receita").toggle(tempoAddTipo);
+    }
+    var $link = $("#show-form-tipo-despesa");
+
+    if ($link.hasClass('is-active')) {
+      $link.removeClass('is-active');
+    }else{
+      $link.addClass('is-active')
+    }
+
+    $(".add-tipo-despesa").toggle(tempoAddTipo);
+  });
+  $("#show-form-tipo-receita").click(function(event) {
+    event.preventDefault();
+    if($(".add-tipo-despesa").css('display') != 'none') {
+
+      $("#show-form-tipo-despesa").removeClass('is-active');
+      $(".add-tipo-despesa").toggle(tempoAddTipo);
+    }
+
+    var $link = $("#show-form-tipo-receita");
+
+    if ($link.hasClass('is-active')) {
+      $link.removeClass('is-active');
+    }else{
+      $link.addClass('is-active')
+    }
+
+    $(".add-tipo-receita").toggle(tempoAddTipo);
   });
 
 });
